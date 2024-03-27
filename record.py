6d3bce3e6e8f78331e28b337835ac64272346b13
Aashish792 <errorak143@gmail.com>
2024-03-26 23:45:08 -0400

    refactored code

--- record.py
+++ record.py
@@ -1,31 +1,25 @@
-import os
-import numpy as np
 import cv2
+from utils import CFEVideoConf
 
-'''
-Looking for record-video.py? Check out lessons/record-video.py
-'''
+def record_video(save_path='saved-media/video.avi', res='720p', fps=24.0):
+    cap = cv2.VideoCapture(0)
+    config = CFEVideoConf(cap, filepath=save_path, res=res)
+    out = cv2.VideoWriter(save_path, config.video_type, fps, config.dims)
 
-from utils import CFEVideoConf, image_resize
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Failed to capture video frame. Exiting...")
+                break
+            out.write(frame)
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(20) & 0xFF == ord('q'):
+                break
+    finally:
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
 
-cap = cv2.VideoCapture(0)
-save_path = 'saved-media/video.avi'
-frames_per_seconds = 24.0
-config = CFEVideoConf(cap, filepath=save_path, res='720p')
-out = cv2.VideoWriter(save_path, config.video_type, frames_per_seconds, config.dims)
-
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    out.write(frame)
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    record_video()
